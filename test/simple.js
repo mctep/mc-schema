d06e0491c970d47d1164fadfd858ea53b31e5a5c
mctep <mctep77@gmail.com>
2014-12-24 16:05:14 +0000

    fixes, schema4

--- test/simple.js
+++ test/simple.js
@@ -1,81 +1,13 @@
 var validator = require('../lib');
 
 validator.validate(
+	{"$ref": "http://json-schema.org/draft-04/schema#"},
+
 	{
-	    "$schema": "http://json-schema.org/draft-04/schema#",
-	    "title": "Product set",
-	    "type": "array",
-	    "items": {
-	        "title": "Product",
-	        "type": "object",
-	        "properties": {
-	            "id": {
-	                "description": "The unique identifier for a product",
-	                "type": "number"
-	            },
-	            "name": {
-	                "type": "string"
-	            },
-	            "price": {
-	                "type": "number",
-	                "minimum": 0,
-	                "exclusiveMinimum": true
-	            },
-	            "tags": {
-	                "type": "array",
-	                "items": {
-	                    "type": "string"
-	                },
-	                "minItems": 1,
-	                "uniqueItems": true
-	            },
-	            "dimensions": {
-	                "type": "object",
-	                "properties": {
-	                    "length": {"type": "number"},
-	                    "width": {"type": "number"},
-	                    "height": {"type": "number"}
-	                },
-	                "required": ["length", "width", "height"]
-	            },
-	            "warehouseLocation": {
-	                "description": "Coordinates of the warehouse with the product"
-	            }
-	        },
-	        "required": ["id", "name", "price"]
-	    }
-	},
-	[
-	    {
-	        "id": 2,
-	        "name": "An ice sculpture",
-	        "price": 12.50,
-	        "tags": ["cold", "ice"],
-	        "dimensions": {
-	            "length": 7.0,
-	            "width": 12.0,
-	            "height": 9.5
-	        },
-	        "warehouseLocation": {
-	            "latitude": -78.75,
-	            "longitude": 20.4
-	        }
-	    },
-	    {
-	        "id": 3,
-	        "name": "A blue mouse",
-	        "price": 25.50,
-	            "dimensions": {
-	            "length": 3.1,
-	            "width": 1.0,
-	            "height": 1.0
-	        },
-	        "warehouseLocation": {
-	            "latitude": 54.4,
-	            "longitude": -32.7
-	        }
-	    }
-	]
+        "definitions": {
+            "foo": {"type": 1}
+        }
+    }
 
 );
 
