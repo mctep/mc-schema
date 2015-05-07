5f0b51680b3e511079699cd325950c6efecdd442
mctep <mctep77@gmail.com>
2015-05-07 11:31:27 +0000

    addValidator fix

--- test/simple.js
+++ test/simple.js
@@ -1,18 +1,23 @@
 var validator = require('../lib');
 
-validator.addType('aaa', function(typeOk) {
-	typeOk = true;
+validator.addValidator('aaa', function(value, type, schema, dataPath, collectErrors) {
+	if (type === 'string' && value.length > schema.aaa) {
+		this.addError({
+			code: 'STRING_LENGTH_LONG',
+			expected: schema.aaa,
+			actual: value.length,
+			dataPath: dataPath
+		});
+		if (!collectErrors) { return value; }
+	}
 });
 
 var schema = validator.compile(
 	{
-		type: 'aaa',
-		items: {
-			type: 'integer'
-		}
+		aaa: 3
 	}
 );
 
 console.log(schema.validate(
-	[1,2,3,2], { collectErrors: true }
+	'1233', { collectErrors: true }
 ));
